294c48a4162481de5dc2a3ddd3ecad7e808d0f25
Asrilhidayat <114537062+Asrilhidayat@users.noreply.github.com>
2023-10-04 23:32:56 +0700

    Add files via upload

---
+++ Tugas.js
@@ -0,0 +1,10 @@
+const firstName = "Asril";  // tipe data string
+const lastName = "Hidayat";  // tipe data string
+const age = 20;  // tipe data number
+const isMarried = false;  // tipe data boolean
+
+// Menampilkan nilai variabel
+console.log("Nama Depan:", firstName);
+console.log("Nama Belakang:", lastName);
+console.log("Umur:", age);
+console.log("Status Pernikahan:", isMarried);
